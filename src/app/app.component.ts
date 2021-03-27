afafc5d856db6d9e2e20cd9919c00c78bf0dcea2
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-27 09:24:08 +0300

    app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onInputLength(e: any) {
+    const target = e.target as HTMLTextAreaElement;
+    const parsedValue = parseInt(target.value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters
+    }
+    if (this.includeNumbers) {
+      validChars += numbers
+    }
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
